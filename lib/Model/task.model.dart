0f9bb0df56c38aa5e63f63dabe16d6102e95d546
MOHAMAD ZAKIR ZIKRI BIN IBRAHIM <80703252+zakirzikri12@users.noreply.github.com>
2022-04-19 16:11:43 +0800

    -container , task, description ,border

---
+++ lib/Model/task.model.dart
@@ -0,0 +1,13 @@
+class Task{
+  String title;
+  String description;
+
+  Task({required this.title , required this.description});
+}
+
+List<Task> tasklist = [
+  Task(title: 'Task 1',description: 'description 1'),
+  Task(title: 'Task 2',description: 'description 1'),
+  Task(title: 'Task 3',description: 'description 1'),
+
+];
--- lib/screen/homescreen.dart
+++ lib/screen/homescreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';   //material.dart
+import 'package:flutter/material.dart';
+
+import '../Model/task.model.dart';   //material.dart
 
 
 
@@ -12,38 +14,57 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar
         (title:Text('APP AKU'),
       ),
-      body:ListView(
-        children: List.generate(tasklist.length, (index) => CustomText(text: tasklist[index]))
-        //
+      body:Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: List.generate(tasklist.length, (index) => TaskContainer(task: tasklist[index])
+        )
+
         // [
         //   CustomText(text: 'Task1'),
         //   CustomText(text : 'Task2'),
         //   CustomText(text : 'Task3'),
         //
         // ],
+
       ),
     );
   }
 }
 
-class CustomText extends StatelessWidget {
-  final String text;
+class TaskContainer extends StatelessWidget {
+  // final String text;
+  final Task task;
 
-  CustomText({required this.text});
+  TaskContainer({required this.task});
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Padding(
       padding: const EdgeInsets.all(8.0),
-      color: Colors.red,
-      child: Text(
-        text,
-        style: TextStyle(fontSize: 30),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.blueGrey.shade100,
+          border: Border.all(color: Colors.grey.shade50) ,
+        ),
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              task.title,
+              style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold) ,
+            ),
+            SizedBox(
+              height: 6,
+            ),
+            Text(
+              task.description,
+              style: TextStyle(fontSize: 20) ,
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
 
-List<String> tasklist = [
-'task 1','Task 2','task 3'
-];
